1c206f163c3dce96fdcc32f1100443a3f414f9bf
Matthew Hildebrand <korvic@live.ca>
2020-05-27 13:07:19 -0500

    Add other races and their default languages.

--- src/main/java/net/korvic/rppersonas/personas/PersonaRace.java
+++ src/main/java/net/korvic/rppersonas/personas/PersonaRace.java
@@ -1,23 +1,20 @@
 package net.korvic.rppersonas.personas;
 
+import lombok.Getter;
+
 public enum PersonaRace {
 
-	HUMAN("Human", new PersonaSubRace[]{ PersonaSubRace.ARMUSIAN, PersonaSubRace.CARRIBARD, PersonaSubRace.GRAVICAN });
+	HUMAN("Human", new PersonaSubRace[] { PersonaSubRace.ARMUSIAN, PersonaSubRace.CARRIBARD, PersonaSubRace.GRAVICAN }),
+	DWARF("Dwarf", new PersonaSubRace[] { PersonaSubRace.CAVE_DWARF, PersonaSubRace.HILL_DWARF, PersonaSubRace.DEEP_DWARF }),
+	REHK("Rehk", new PersonaSubRace[] { PersonaSubRace.REHK_UR, PersonaSubRace.REHK_UG }),
+	ELF("Elf", new PersonaSubRace[] { PersonaSubRace.SPRING_ELF, PersonaSubRace.SUMMER_ELF, PersonaSubRace.AUTUMN_ELF, PersonaSubRace.WINTER_ELF });
 
-	private String name;
-	private PersonaSubRace[] subRaceList;
+	@Getter private String name;
+	@Getter private PersonaSubRace[] subRaceList;
 
 	PersonaRace(String name, PersonaSubRace[] subRaces) {
 		this.name = name;
 		this.subRaceList = subRaces;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public PersonaSubRace[] getSubRaces() {
-		return subRaceList;
 	}
 
 	public static PersonaRace getByName(String name) {
--- src/main/java/net/korvic/rppersonas/personas/PersonaSubRace.java
+++ src/main/java/net/korvic/rppersonas/personas/PersonaSubRace.java
@@ -7,16 +7,22 @@ public enum PersonaSubRace {
 	// HUMANS
 	ARMUSIAN("Armusian", 90, PersonaRace.HUMAN, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.ARMUSIAN }),
 	CARRIBARD("Carribard", 90, PersonaRace.HUMAN, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.CARRIB }),
-	GRAVICAN("Gravican", 90, PersonaRace.HUMAN, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.GRAVICAN });
+	GRAVICAN("Gravican", 90, PersonaRace.HUMAN, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.GRAVICAN }),
 
 	// DWARVES
-
+	CAVE_DWARF("Cave Dwarf", 90, PersonaRace.DWARF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.YAZYK }),
+	HILL_DWARF("Hill Dwarf", 90, PersonaRace.DWARF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.YAZYK }),
+	DEEP_DWARF("Deep Dwarf", 90, PersonaRace.DWARF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.YAZYK }),
 
 	// REHK
-
+	REHK_UR("Rehk-Ur", 90, PersonaRace.REHK, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.REHKISH }),
+	REHK_UG("Rehk-Ug", 90, PersonaRace.REHK, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.REHKISH }),
 
 	// ELVES
-
+	SPRING_ELF("Spring Elf", 90, PersonaRace.ELF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.CLASS_ELVEN }),
+	SUMMER_ELF("Summer Elf", 90, PersonaRace.ELF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.CLASS_ELVEN }),
+	AUTUMN_ELF("Autumn Elf", 90, PersonaRace.ELF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.NEW_ELVEN }),
+	WINTER_ELF("Winter Elf", 90, PersonaRace.ELF, new PersonaLanguage[] { PersonaLanguage.COMMON, PersonaLanguage.NEW_ELVEN });
 
 	@Getter private String name;
 	@Getter private int maxAge;
