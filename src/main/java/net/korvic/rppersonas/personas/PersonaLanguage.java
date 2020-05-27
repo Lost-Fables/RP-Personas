c4c671be82c348b60299d3192a682b177dd6246d
Matthew Hildebrand <korvic@live.ca>
2020-05-27 01:13:47 -0500

    MOAR LANGUAGES since Lugh sent me a few more before I yeet myself into night coma.

--- src/main/java/net/korvic/rppersonas/personas/PersonaLanguage.java
+++ src/main/java/net/korvic/rppersonas/personas/PersonaLanguage.java
@@ -6,26 +6,37 @@ public enum PersonaLanguage {
 
 	ANC_COMMON("Ancient Common", "[AC]"),
 	COMMON("Common", ""),
+
 	GRAVICAN("Gravican", "[GR]"),
 	ARMUSIAN("Armusian", "[MS]"),
 	CARRIB("Carrib", "[CB]"),
+
 	ANC_REHKISH("Ancient Rehkish", "[AR]"),
 	REHKISH("Rehkish", "[R]"),
+
 	ANC_ELVEN("Ancient Elven", "[AE]"),
 	CLASS_ELVEN("Classical Elven", "[E]"),
 	NEW_ELVEN("New Elven", "[NE]"),
+
 	ANC_YAZYK("Ancient Yazyk", "[AY]"),
 	YAZYK("Yazyk", "[Y]"),
+
 	DEMONIC("Demonic", "[D]"),
 	PRIMORDIAL("Primordial", "[P]"),
 	CELESTIAL("Celestial", "[C]"),
 	DRUII_TONGUE("Druii Tongue", "[DT]"),
 	HIEROPHANT("Hierophant", "[H]"),
 	TALIDURIAN("Talidurian", "[T]"),
+
 	PYRAN("Pyran", "[PY]"),
 	AQUAN("Aquan", "[AQ]"),
 	ILLUMAN("Illuman", "[IL]"),
-	STYGAN("Stygan", "[SY]");
+	STYGAN("Stygan", "[SY]"),
+
+	JAVALI("Javali", "[JV]"),
+	MAKSHA("Maksha", "[MK]"),
+	GAHORIAN("Gahorian", "[GH]"),
+	VERIVAN("Verivan", "[VR]");
 
 	@Getter private String name;
 	@Getter private String tag;
