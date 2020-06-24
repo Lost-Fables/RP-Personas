401e6d758a1e242fece048d9924bbede098e05c1
Matthew Hildebrand <korvic@live.ca>
2020-06-23 20:54:27 -0500

    Formatting updates for app accepting. Add button for accept/deny.

--- src/main/java/net/korvic/rppersonas/commands/RezCommands.java
+++ src/main/java/net/korvic/rppersonas/commands/RezCommands.java
@@ -26,7 +26,9 @@ public class RezCommands extends BaseCommand {
 		list.add(new TextComponent(RPPersonas.SECONDARY_DARK + "" + ChatColor.UNDERLINE + "----------- Rez Apps -----------\n"));
 
 		for (RezApp app : plugin.getRezHandler().getRezAppList().values()) {
-			TextComponent text = new TextComponent("ID: " + app.getPersonaID() + " Karma: " + app.getKarma() + " Kills/Deaths: " + app.getKills() + "/" + app.getDeaths());
+			TextComponent text = new TextComponent(RPPersonas.PRIMARY_DARK + "ID: " + RPPersonas.SECONDARY_DARK + app.getPersonaID() +
+												   RPPersonas.PRIMARY_DARK + " Karma: " + RPPersonas.SECONDARY_DARK + app.getKarma() +
+												   RPPersonas.PRIMARY_DARK + " Kills/Deaths: " + RPPersonas.SECONDARY_DARK + app.getKills() + "/" + app.getDeaths());
 			ClickEvent event = new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rpp rez info " + app.getPersonaID());
 
 			text.setHoverEvent(MessageUtil.hoverEvent("Click to see their responses."));
@@ -45,20 +47,33 @@ public class RezCommands extends BaseCommand {
 		List<TextComponent> list = new ArrayList<>();
 		list.add(new TextComponent(RPPersonas.SECONDARY_DARK + "" + ChatColor.UNDERLINE + "----------- Rez Apps -----------\n"));
 
-		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "Why are you seeking resurrection?\n"));
+		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "Why are you seeking resurrection?"));
 		list.add(new TextComponent(RPPersonas.SECONDARY_LIGHT + app.getResponses().getResponse(1)));
 		list.add(new TextComponent(""));
 
-		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "Have you lived an honest life?\n"));
+		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "Have you lived an honest life?"));
 		list.add(new TextComponent(RPPersonas.SECONDARY_LIGHT + app.getResponses().getResponse(2)));
 		list.add(new TextComponent(""));
 
-		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "What is the meaning of your life?\n"));
+		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "What is the meaning of your life?"));
 		list.add(new TextComponent(RPPersonas.SECONDARY_LIGHT + app.getResponses().getResponse(3)));
 		list.add(new TextComponent(""));
 
-		list.add(new TextComponent(RPPersonas.PRIMARY_DARK + "If you believe this persona worth rezzing, please use " + RPPersonas.SECONDARY_DARK + "/rpp rez accept " + app.getPersonaID() + RPPersonas.PRIMARY_DARK +
-								   ".\n Otherwise, to deny this persona resurrection use " + RPPersonas.SECONDARY_DARK + "/rpp rez deny " + app.getPersonaID()));
+		{
+			TextComponent acceptText = new TextComponent(RPPersonas.PRIMARY_DARK + "If you believe this persona worth rezzing, please use/click " + RPPersonas.SECONDARY_DARK + "/rpp rez accept " + app.getPersonaID());
+			ClickEvent event = new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rpp rez accept " + app.getPersonaID());
+			acceptText.setHoverEvent(MessageUtil.hoverEvent("Click to accept this app."));
+			acceptText.setClickEvent(event);
+			list.add(acceptText);
+		}
+
+		{
+			TextComponent denyText = new TextComponent(RPPersonas.PRIMARY_DARK + "Otherwise, to deny this persona resurrection use/click " + RPPersonas.SECONDARY_DARK + "/rpp rez deny " + app.getPersonaID());
+			ClickEvent event = new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rpp rez deny " + app.getPersonaID());
+			denyText.setHoverEvent(MessageUtil.hoverEvent("Click to deny this app."));
+			denyText.setClickEvent(event);
+			list.add(denyText);
+		}
 
 		for (TextComponent text : list) {
 			msg(text);
